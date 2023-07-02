e8a32fb30ec4b022302b057e17ae7fb8d4f6118c
Jainil Patel <123730303+jjparh@users.noreply.github.com>
2023-07-02 13:26:30 +0100

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "aws" {
   region            = var.aws_region
-  access_key        = var.access_key
-  secret_key        = var.secret_key 
+  #access_key        = var.access_key
+  #secret_key        = var.secret_key 
 }
 
 terraform {
