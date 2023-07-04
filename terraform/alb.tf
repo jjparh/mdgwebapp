2aef7a93e7e0a62550a22f06fcf60c0db266a8da
jjparh <jjp_emz@yahoo.co.uk>
2023-07-04 22:15:55 +0100

    alb arn

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -29,7 +29,7 @@ resource "aws_alb_listener" "front_end" {
   protocol          = "HTTP"
 
   default_action {
-    target_group_arn = aws_alb_target_group.app.id
+    target_group_arn = aws_alb_target_group.app.arn
     type             = "forward"
   }
 }
