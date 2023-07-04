6396195bb990719c15bf4d4016f09e8b1382c353
jjparh <jjp_emz@yahoo.co.uk>
2023-07-04 21:18:02 +0100

    cpu/mem increased

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -81,13 +81,13 @@ variable "aws_ecs_task_def_fam" {
 variable "fargate_cpu" {
   type        = number
   description = "cpus"
-  default     = 1024
+  default     = 2048
 }
 
 variable "fargate_memory" {
   type        = number
   description = "memory"
-  default     = 2048
+  default     = 4096
 }
 
 variable "aws_ecs_service_name" {
